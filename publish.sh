d28533fbb37b0cce93d7834a0fa452e2d1f9fd3d
Sergei Silnov <po@kumekay.com>
2018-10-14 21:17:03 +0200

    Add more tags to docker

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-export RESTIC_VERSION="0.9.3"
-docker build -t hortio/restic:latest -t hortio/restic:"v${RESTIC_VERSION}" .
+export RESTIC_MAJOR="0"
+export RESTIC_MINOR="9"
+export RESTIC_PATCH="3"
+docker build -t hortio/restic:latest \
+    -t hortio/restic:"v${RESTIC_MAJOR}.${RESTIC_MINOR}.${RESTIC_PATCH}" \
+    -t hortio/restic:"v${RESTIC_MAJOR}.${RESTIC_MINOR}" \
+    -t hortio/restic:"v${RESTIC_MAJOR}" .
 docker push hortio/restic:latest
-docker push hortio/restic:"v${RESTIC_VERSION}"
+docker push hortio/restic:"v${RESTIC_MAJOR}.${RESTIC_MINOR}.${RESTIC_PATCH}"
+docker push hortio/restic:"v${RESTIC_MAJOR}.${RESTIC_MINOR}"
+docker push hortio/restic:"v${RESTIC_MAJOR}"
