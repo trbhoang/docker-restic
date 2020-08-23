c844bb6d6cb5cd2c87b1e37b962830fc5b311d44
Hoang Tran <trbhoang@gmail.com>
2020-08-23 15:42:42 +0700

    dump backup logs to output console

--- backup.sh
+++ backup.sh
@@ -1,49 +1,33 @@
 #!/bin/sh
 
-lastLogfile="/var/log/backup-last.log"
-
-copyErrorLog() {
-  cp ${lastLogfile} /var/log/backup-error-last.log
-}
-
-logLast() {
-  echo "$1" >> ${lastLogfile}
-}
-
 start=`date +%s`
-rm -f ${lastLogfile}
+
 echo "Starting Backup at $(date +"%Y-%m-%d %H:%M:%S")"
-echo "Starting Backup at $(date)" >> ${lastLogfile}
-logLast "BACKUP_CRON: ${BACKUP_CRON}"
-logLast "RESTIC_TAG: ${RESTIC_TAG}"
-logLast "RESTIC_FORGET_ARGS: ${RESTIC_FORGET_ARGS}"
-logLast "RESTIC_JOB_ARGS: ${RESTIC_JOB_ARGS}"
+echo "BACKUP_CRON: ${BACKUP_CRON}"
+echo "RESTIC_FORGET_ARGS: ${RESTIC_FORGET_ARGS}"
+echo "RESTIC_JOB_ARGS: ${RESTIC_JOB_ARGS}"
 
-
-# Do not save full backup log to logfile but to backup-last.log
-restic backup /data ${RESTIC_JOB_ARGS} --tag=${RESTIC_TAG?"Missing environment variable RESTIC_TAG"} >> ${lastLogfile} 2>&1
+restic backup /data ${RESTIC_JOB_ARGS} 2>&1
 rc=$?
-logLast "Finished backup at $(date)"
+echo "Finished backup at $(date)"
 if [[ $rc == 0 ]]; then
-    echo "Backup Successfull" 
+    echo "Backup Successfull"
 else
     echo "Backup Failed with Status ${rc}"
     restic unlock
-    copyErrorLog
     kill 1
 fi
 
 if [ -n "${RESTIC_FORGET_ARGS}" ]; then
     echo "Forget about old snapshots based on RESTIC_FORGET_ARGS = ${RESTIC_FORGET_ARGS}"
-    restic forget ${RESTIC_FORGET_ARGS} >> ${lastLogfile} 2>&1
+    restic forget ${RESTIC_FORGET_ARGS} 2>&1
     rc=$?
-    logLast "Finished forget at $(date)"
+    echo "Finished forget at $(date)"
     if [[ $rc == 0 ]]; then
         echo "Forget Successfull"
     else
         echo "Forget Failed with Status ${rc}"
         restic unlock
-        copyErrorLog
     fi
 fi
 
